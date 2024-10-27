eccbce38d73a00d62adbf9bbb174dabb7ce6790a
Chaemin-L <chaem12@kookmin.ac.kr>
2024-10-27 16:44:49 +0900

    feat: Account api 에러 처리 및 response 통일

--- user/src/main/java/com/example/user/account/service/AccountServiceImpl.java
+++ user/src/main/java/com/example/user/account/service/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.user.account.service;
 import com.example.user.account.dto.TransactionRequest;
 import com.example.user.account.entity.Account;
 import com.example.user.account.repository.AccountRepository;
+import com.example.user.global.CustomException;
 import com.example.user.user.entity.User;
 import com.example.user.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +21,7 @@ public class AccountServiceImpl implements AccountService {
     public Account getMyAccount(String userId) {
         User user = userRepository.findById(userId).orElseThrow();
         Optional<Account> account = accountRepository.findAccountByUser(user);
-        if (account.isEmpty()) throw new RuntimeException("Account not found");
+        if (account.isEmpty()) throw new CustomException(404, "존재하지 않는 계좌입니다");
         return account.get();
     }
 
@@ -35,7 +36,7 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public Account withdrawal(String userId, TransactionRequest request){
         Account account = getMyAccount(userId);
-        if(account.getBalance() < request.amount()) throw new RuntimeException("Account balance not enough");
+        if(account.getBalance() < request.amount()) throw new CustomException(400,"잔액이 부족합니다");
         account.changeBalance(-request.amount());
         accountRepository.save(account);
         return account;

393180989fcf26c70ca4a69b5a1660e3fdeeff0b
singersongha <songha714@naver.com>
2024-10-29 12:30:14 +0900

    chore: 수정

--- review/src/main/java/com/example/review/controller/ReviewController.java
+++ review/src/main/java/com/example/review/controller/ReviewController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 public class ReviewController {
 
